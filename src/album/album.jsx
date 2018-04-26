40d7b9a7a7b67759b5b425704f1dd47c65222cfb
matheus-lima92 <lima.eca@gmail.com>
2018-04-26 00:51:01 -0300

    table with rating stars

--- src/album/album.jsx
+++ src/album/album.jsx
@@ -9,28 +9,57 @@ export default class Album extends Component{
 		this.state = {...this.getAlbumInfo()}
 		this.sortByDescription = this.sortByDescription.bind(this)
 		this.sortByRating = this.sortByRating.bind(this)
+		this.starHover = this.starHover.bind(this)
+		this.starClick = this.starClick.bind(this)
 		this.renderTableRows();
 	}
 
 	getAlbumInfo(){
-		let queryPosition = window.location.href.indexOf('?')
-		let queryLength = window.location.href.length
-		let albumID = window.location.href.substring(queryPosition+1, queryLength)
-		let albumInfo = albumsData.find(album => album.id === albumID)
+		const queryPosition = window.location.href.indexOf('?')
+		const queryLength = window.location.href.length
+		const albumID = window.location.href.substring(queryPosition+1, queryLength)
+		const albumInfo = albumsData.find(album => album.id === albumID)
 		return albumInfo;
 	}
 
 	sortByDescription(){
-		let pics = this.state.pictures;
+		const pics = this.state.pictures;
 		pics.sort((a,b) => a.description.toLowerCase() < b.description.toLowerCase())
 		this.setState({...this.state, pictures: pics})
 
 	}
 
 	sortByRating(){
-		let pics = this.state.pictures;
+		const pics = this.state.pictures;
 		pics.sort((a,b) => a.rating < b.rating)
 		this.setState({...this.state, pictures: pics})
+	}
+
+	starHover(){
+		console.log("Hover star!")
+	}
+
+	starClick(){
+		console.log("star clicked!")
+	}
+
+	renderRatingStars(rating, pictureID){
+		let auxArray = []
+		for(var i=0; i < Math.floor(rating); i++){
+			auxArray[i] = 1;
+		}
+		//handle decimal ratings
+		const decimalRating = rating % 1;
+		if(decimalRating >= 0.25 && decimalRating < 0.75)
+			auxArray[i] = 0.5;
+		else if(decimalRating > 0.75)
+			auxArray[i] = 1;
+		return auxArray.map((rating, index) => (
+			(rating === 1) ? 
+				<Icon key={`ID${pictureID}_ratingStar${index}`} icon='star' onMouseEnter={() => this.starHover()} onClick={() => this.starHover()}/> : 
+				<Icon key={`ID${pictureID}_ratingStar${index}`} icon='star-half' onMouseEnter={() => this.starHover()} onClick={() => this.starHover()}/>
+
+		))
 	}
 
 	renderTableRows(){
@@ -42,8 +71,8 @@ export default class Album extends Component{
 				<td>
 					<span>{pic.description.length > 15 ? `${pic.description.substring(0,35)}...` : pic.description}</span>
 				</td>
-				<td>
-					<span>{pic.rating}</span>
+				<td key={`rating_${pic.id}`}>
+					{this.renderRatingStars(pic.rating, pic.id)}
 				</td>
 			</tr>
 		))
@@ -61,10 +90,12 @@ export default class Album extends Component{
 					        </th>
 					        <th>
 					        	<span>Description</span>
-					        	<Icon icon='angle-down' style='table-icon' onClick={() => this.sortByDescription()}></Icon></th>
+					        	<Icon icon='angle-down' style='table-icon' onClick={() => this.sortByDescription()} />
+					        </th>
 					        <th>
 					        	<span>Rating</span>
-					        	<Icon icon='angle-down' style='table-icon' onClick={() => this.sortByRating()}></Icon></th>
+					        	<Icon icon='angle-down' style='table-icon' onClick={() => this.sortByRating()} />
+					        </th>
       					</tr>
 					</thead>
 					<tbody>
--- src/common/iconButton.jsx
+++ src/common/iconButton.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default props => (
-    <i className={`fa fa-${props.icon} ${props.style}`} onClick={props.onClick}></i>
+    <i className={`fa fa-${props.icon} ${props.style}`} onClick={props.onClick} onMouseEnter={props.onMouseEnter}></i>
 )
