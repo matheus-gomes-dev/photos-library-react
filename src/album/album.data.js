3cc4d3e310f7c01cd34b70378e13024aef052090
matheus-lima92 <lima.eca@gmail.com>
2018-04-25 21:31:18 -0300

    initial table of pictures implementation

--- src/album/album.data.js
+++ src/album/album.data.js
@@ -4,19 +4,32 @@ const albumsData = [
 		title: 'Beautiful Nature',
 		pictures: [
 			{
-				url: '../../public/img/pic1.jpg',
+				url: 'http://prmurphy.com.au/wp-content/uploads/2015/10/pexels-photo-236047.jpg',
 				rating: 5,
-				votes: 3
+				votes: 3,
+				id: 1,
+				description: 'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.'
 			},
 			{
-				url: '../../public/img/pic2.jpg',
+				url: 'http://images.all-free-download.com/images/graphiclarge/sky_sunset_nature_216079.jpg',
 				rating: 3,
-				votes: 5
+				votes: 5,
+				id: 2,
+				description: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.'
 			},
 			{
-				url: '../../public/img/pic3.jpg',
+				url: 'https://www.visitportugal.com/sites/www.visitportugal.com/files/mediateca/23_660x371.jpg',
 				rating: 4.5,
-				votes: 10
+				votes: 10,
+				id: 3,
+				description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+			},
+			{
+				url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT50i2xysIED8QIVLHYMQZmGiQlrXuFBBxCtJhdaUlGqRfjqrZixQ',
+				rating: 4.5,
+				votes: 10,
+				id: 4,
+				description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
 			}
 		]
 	},
@@ -25,14 +38,25 @@ const albumsData = [
 		title: 'Funny Animals',
 		pictures: [
 			{
-				url: '../../public/img/pic4.jpg',
+				url: 'https://kids.nationalgeographic.com/content/dam/kids/photos/articles/Other%20Explore%20Photos/R-Z/Wacky%20Weekend/Funny%20Animal%20Faces/ww-funny-animal-faces-hippopotamus.adapt.945.1.jpg',
 				rating: 5,
-				votes: 3
+				votes: 3,
+				id: 5,
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
 			},
 			{
-				url: '../../public/img/pic5.jpg',
+				url: 'https://edge.alluremedia.com.au/uploads/businessinsider/2015/12/nothing-weird-about-a-pants-less-madagascan-sifaki-busting-out-some-solodance-moves-in-the-desert.jpg',
 				rating: 3,
-				votes: 5
+				votes: 5,
+				id: 6,
+				description: ''
+			},
+			{
+				url: 'https://kids.nationalgeographic.com/content/dam/kids/photos/articles/Other%20Explore%20Photos/R-Z/Wacky%20Weekend/Funny%20Animal%20Faces/ww-funny-animal-faces-goat.adapt.945.1.jpg',
+				rating: 3,
+				votes: 5,
+				id: 7,
+				description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.'
 			}
 		]
 	},
@@ -41,9 +65,11 @@ const albumsData = [
 		title: 'Amazing Buildings',
 		pictures: [
 			{
-				url: '../../public/img/pic6.jpg',
+				url: 'http://zmeeed.info/wp-content/uploads/2017/03/modern-style-architecture-cool-cute-cool-real-architecture-buildings-futuristic-b-baihusi-com.jpg',
 				rating: 5,
-				votes: 3
+				votes: 3,
+				id: 8,
+				description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
 			}
 		]
 	}
--- src/album/album.jsx
+++ src/album/album.jsx
@@ -6,6 +6,7 @@ export default class Album extends Component{
 	constructor(props){
 		super(props)
 		this.state = {...this.getAlbumInfo()}
+		this.renderTableRows();
 	}
 
 	getAlbumInfo(){
@@ -16,10 +17,38 @@ export default class Album extends Component{
 		return albumInfo;
 	}
 
+	renderTableRows(){
+		return this.state.pictures.map(pic => (
+			<tr key={`row_${pic.id}`}>
+				<td>
+					<img src={pic.url} width='50' height='50'/>
+				</td>
+				<td>
+					<span>{pic.description.length > 15 ? `${pic.description.substring(0,35)}...` : pic.description}</span>
+				</td>
+				<td>
+					<span>{pic.rating}</span>
+				</td>
+			</tr>
+		))
+	}
+
 	render(){
 		return(
 			<div>
 				<h3>{this.state.title}</h3>
+				<table className='table'>
+					<thead>
+						<tr>
+					        <th>Thumbnail</th>
+					        <th>Title</th>
+					        <th>Rating</th>
+      					</tr>
+					</thead>
+					<tbody>
+						{this.renderTableRows()}
+					</tbody>
+				</table>
 			</div>
 		)
 	}
